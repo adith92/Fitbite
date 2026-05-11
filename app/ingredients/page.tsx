1bcd93429f3962703e4881b70afc0253b5f2fee1
TritiumTech <adithyapratama2101@gmail.com>
2026-05-11 15:04:14 +0700

    Add ingredients page

---
+++ app/ingredients/page.tsx
@@ -0,0 +1,5 @@
+import Link from "next/link";
+
+export default function Page() {
+  return <main><Link href="/">Kembali</Link><h1>Ingredients</h1><p>Input bahan rumah dan dapatkan rekomendasi resep sehat.</p></main>;
+}
