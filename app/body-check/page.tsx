dbc394e9d397d7ed1d7253db8783ca7b39ab9980
TritiumTech <adithyapratama2101@gmail.com>
2026-05-11 15:03:39 +0700

    Add body check

---
+++ app/body-check/page.tsx
@@ -0,0 +1,5 @@
+import Link from "next/link";
+
+export default function Page() {
+  return <main><Link href="/">Kembali</Link><h1>Body Check</h1><p>Hitung BMI, kalori harian, dan macro target.</p></main>;
+}
